775348fe941cee4cadb1ba4a66a0818782b48066
101zh <67253838+101zh@users.noreply.github.com>
2023-11-09 01:29:40 +0000

    feat(Percolation.java): Finish

---
+++ src/main/java/Percolation.java
@@ -0,0 +1,120 @@
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    private WeightedQuickUnionUF quickUnionFind;
+    private boolean[][] grid;
+    private int[][] numberedGrid;
+    private int len;
+    private int virtualTop;
+    private int virtualBottom;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        grid = new boolean[n][n];
+        numberedGrid = new int[n][n];
+        quickUnionFind = new WeightedQuickUnionUF((n * n) + 2);
+        virtualTop = quickUnionFind.count() - 2;
+        virtualBottom = quickUnionFind.count() - 1;
+        len = n;
+
+        int curNumber = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                numberedGrid[i][j] = curNumber;
+                curNumber++;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            quickUnionFind.union(i, virtualTop);
+        }
+
+        for (int i = virtualTop - len; i < virtualTop; i++) {
+            quickUnionFind.union(i, virtualBottom);
+        }
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        row -= 1;
+        col -= 1;
+        throwException(isValid(row, col));
+        grid[row][col] = true;
+
+        if (isValid(row - 1, col) && grid[row - 1][col]) {
+            quickUnionFind.union(numberedGrid[row][col], numberedGrid[row - 1][col]);
+        }
+        if (isValid(row, col - 1) && grid[row][col - 1]) {
+            quickUnionFind.union(numberedGrid[row][col], numberedGrid[row][col - 1]);
+        }
+        if (isValid(row, col + 1) && grid[row][col + 1]) {
+            quickUnionFind.union(numberedGrid[row][col], numberedGrid[row][col + 1]);
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        throwException(isValid(row, col));
+
+        return grid[row - 1][col - 1];
+
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        throwException(isValid(row, col));
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        int openSites = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (grid[i][j]) {
+                    openSites++;
+                }
+            }
+        }
+        return openSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return quickUnionFind.connected(virtualTop, virtualBottom);
+    }
+
+    public boolean isValid(int row, int col) {
+        boolean isvalidRows = row >= 0 && row <= len - 1;
+        boolean isvalidCols = col >= 0 && col <= len - 1;
+        return isvalidCols && isvalidRows;
+    }
+
+    public static void throwException(boolean valid) {
+        if (!valid) {
+            throw new IllegalArgumentException("Row or Column is invalid");
+        }
+    }
+
+    // // test client (optional)
+    // public static void main(String[] args) {
+    // Percolation percolation = new Percolation(4);
+    // System.out.println(percolation.quickUnionFind.count());
+    // percolation.open(1, 1);
+    // percolation.open(2, 1);
+    // percolation.open(2, 2);
+    // percolation.open(2, 3);
+    // percolation.open(3, 3);
+    // percolation.open(3, 4);
+    // percolation.open(4, 4);
+
+    // System.out.println(Arrays.deepToString(percolation.grid).replace("], ",
+    // "]\n").replace("[[", "["));
+    // System.out.println(percolation.numberOfOpenSites());
+    // System.out.println(percolation.percolates());
+
+    // }
+
+}
